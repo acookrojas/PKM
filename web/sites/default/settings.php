4d6f0770c9c729aac76d8b54dbf5c459ffb9b34f
Amanda Cook <acookr@gmail.com>
2023-03-07 18:03:10 -0300

    Olivero prueba 2

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -778,3 +778,6 @@ if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+//TOME settings
+$settings['tome_static_cache_exclude'] = ['/'];
